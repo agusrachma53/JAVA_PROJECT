57f1d8b4251936430a60fd2b220e8781ff1ecf64
Agus Rachman <agusrachman@helio.id>
2020-12-09 11:22:10 +0700

    Tipe data Character

---
+++ Documents/Belajar/JAVA_PROJECT/src/main/java/TipeDataCharacter.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agus
+ */
+public class TipeDataCharacter {
+    
+    public static void main(String[] args) {
+            
+        char a = 'A';
+        char g = 'G';
+        char u = 'U';
+        char s = 'S';
+        
+        System.out.println(a);
+        System.out.println(g);
+        System.out.println(u);
+        System.out.println(s);
+        
+    }
+    
+}
