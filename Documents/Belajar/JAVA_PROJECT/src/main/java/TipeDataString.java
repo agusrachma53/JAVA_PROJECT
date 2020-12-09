b3f8331bcb0b046d91558f564fc36397bb19be7f
Agus Rachman <agusrachman@helio.id>
2020-12-09 13:04:22 +0700

    String

---
+++ Documents/Belajar/JAVA_PROJECT/src/main/java/TipeDataString.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author agus
+ */
+public class TipeDataString {
+    
+    public static void main(String[] args) {
+        
+        String firstName = "Agus";
+        
+        String lastName = "Rachman";
+        
+        String fullName = firstName + " " + lastName;
+        
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(fullName);
+        
+        
+    }
+    
+}
